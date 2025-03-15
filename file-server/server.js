03f11bd096247642937019432a58446de334f4aa
jayjijai <jaypangalay@gmail.com>
2025-03-16 00:15:55 +0800

    server.js

--- file-server/server.js
+++ file-server/server.js
@@ -20,6 +20,8 @@ app.get("/download", (req, res) => {
 });
 
 // Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
